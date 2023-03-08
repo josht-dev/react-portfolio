8f4e78405b6a569a1be37fb22fb4ea2dfbd6ec09
Joshua Taylor <toasterrage@gmail.com>
2023-03-08 12:42:45 -0700

    Added imports and react render

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// *****Import Modules*****
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Render the component
+ReactDOM.render(<App />, document.getElementById('root'));
