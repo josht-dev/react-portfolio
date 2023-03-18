a4a8055f7d8fb823dfcac83bfa9365922444145a
Joshua Taylor <toasterrage@gmail.com>
2023-03-18 13:12:02 -0600

    Updated with comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import MainContainer from './components/MainContainer';
 
-
+// Uses the 'MainContainer.js' file to handle page switching
 const App = () => <MainContainer />;
 
 export default App;
