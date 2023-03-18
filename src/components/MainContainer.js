d3c1a2e69f830a74c5c1e17f4ae461263b391c91
Joshua Taylor <toasterrage@gmail.com>
2023-03-18 13:15:38 -0600

    Added imports and started export function

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+// *****Import Modules*****
+import React, { useState } from 'react';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
+import Work from './pages/Work';
+
+// Export function that handles page switching
+export default function MainContainer() {
+  
+}
