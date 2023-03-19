f69565d676c9413fe4608405be5db683deba4351
Joshua Taylor <toasterrage@gmail.com>
2023-03-18 21:07:46 -0600

    fixed icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 // *****Import Modules*****
 import React from 'react';
 import '../styles/Footer.css';
+import phoneIcon from '../assets/icons/phone_icon.svg';
+import emailIcon from '../assets/icons/email_icon.svg';
+import githubIcon from '../assets/icons/GitHub_icon.svg';
+import linkedinIcon from '../assets/icons/LinkedIn_icon.svg';
 
 // Export react component
 export default function Footer() {
@@ -8,19 +12,19 @@ export default function Footer() {
     <footer id='contact-me'>
       <ul>
         <li id='phone' className='show-contact'>
-          <img src="%PUBLIC_URL%/icons/phone_icon.svg" alt="phone icon"></img>
+          <img src={phoneIcon} alt="phone icon"></img>
         </li>
         <li id='email' className='show-contact'>
-          <img src="%PUBLIC_URL%/icons/email_icon.svg" alt="email icon"></img>
+          <img src={emailIcon} alt="email icon"></img>
         </li>
         <li>
           <a href="https://github.com/josht-dev" target="_blank" rel="noreferrer">
-            <img src="%PUBLIC_URL%/icons/GitHub_icon.svg" alt="github icon"></img>
+            <img src={githubIcon} alt="github icon"></img>
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/joshua-taylor-43ab14145" target="_blank" rel="noreferrer">
-            <img src="%PUBLIC_URL%/icons/LinkedIn_icon.svg" alt="linkedin icon"></img>
+            <img src={linkedinIcon} alt="linkedin icon"></img>
           </a>
         </li>
       </ul>
