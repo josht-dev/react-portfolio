fd14437fdc457fe03f3858e094280087bc5706e0
Joshua Taylor <toasterrage@gmail.com>
2023-03-18 20:41:01 -0600

    Added footer code

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+// *****Import Modules*****
+import React from 'react';
+
+// Export react component
+export default function Footer() {
+  return (
+    <footer id='contact-me'>
+      <ul>
+        <li id='phone' className='show-contact'>
+          <img src="%PUBLIC_URL%/icons/phone_icon.svg" alt="phone icon"></img>
+        </li>
+        <li id='email' className='show-contact'>
+          <img src="%PUBLIC_URL%/icons/email_icon.svg" alt="email icon"></img>
+        </li>
+        <li>
+          <a href="https://github.com/josht-dev" target="_blank" rel="noreferrer">
+            <img src="%PUBLIC_URL%/icons/GitHub_icon.svg" alt="github icon"></img>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/joshua-taylor-43ab14145" target="_blank" rel="noreferrer">
+            <img src="%PUBLIC_URL%/icons/LinkedIn_icon.svg" alt="linkedin icon"></img>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
