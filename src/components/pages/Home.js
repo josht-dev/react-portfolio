75b9d53bfaba3c51e0ffdb24c587c9c99d6f5383
Joshua Taylor <toasterrage@gmail.com>
2023-03-19 17:02:25 -0600

    Added home component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+// *****Import Modules*****
+import React from 'react';
+
+/*
+  This page/component is currently empty per the design to only show the
+  header and navigation components on the homepage
+*/
+export default function Home() {
+  return (
+    <div></div>
+  )
+}
