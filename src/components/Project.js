e54f709257fba4732f3670ec3d623a9aea9d25b5
Joshua Taylor <toasterrage@gmail.com>
2023-03-20 00:49:06 -0600

    Added project component

---
+++ src/components/Project.js
@@ -0,0 +1,41 @@
+// *****Import Modules*****
+import React from 'react';
+
+// Export react component
+export default function Project(props) {
+  // Component styling
+  const styles = {
+    projectStyle: {
+      width: props.projectSize
+    },
+    titleStyle: {
+      position: 'absolute',
+      bottom: '50px',
+      zIndex: 2,
+      backgroundColor: '#89AD6D',
+      color: '#FBFAF8',
+      width: 'fit-content',
+      padding: '15px',
+      fontSize: '20px'
+    },
+    imgStyle: {
+      width: '100%',
+      overflow: 'hidden',
+      objectFit: 'none',
+      objectPosition: props.imgPosition,
+      filter: 'blur(5px) brightness(60%)',
+      height: props.imgHeight
+    }
+  }
+
+  return (
+    <div style={styles.projectStyle}>
+      <div style={styles.titleStyle}>
+        <h2>{props.title}</h2>
+      </div>
+      <a href={props.href} target='_blank' rel="noreferrer">
+        <img style={styles.imgStyle} src={props.link} alt={props.alt}></img>
+      </a>
+    </div>
+  );
+}
