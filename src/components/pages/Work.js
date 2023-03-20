f096fbfee6c3ffdb9a919c96e4c62812eecf992f
Joshua Taylor <toasterrage@gmail.com>
2023-03-19 22:52:36 -0600

    Started Work page component

---
+++ src/components/pages/Work.js
@@ -0,0 +1,18 @@
+// *****Import Modules*****
+import React from 'react';
+
+// Component styling
+const styles = {
+  sectionStyle: {
+    width: '100%'
+  }
+}
+
+// Export react component
+export default function Work() {
+  return (
+    <section style={styles.sectionStyle}>
+
+    </section>
+  );
+}
