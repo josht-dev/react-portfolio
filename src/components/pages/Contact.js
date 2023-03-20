2246493070028170e4345cb39e3a4b8c63fd7ed3
Joshua Taylor <toasterrage@gmail.com>
2023-03-19 22:44:51 -0600

    Added Contact page component

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,52 @@
+// *****Import Modules*****
+import React from 'react';
+
+// Component styling
+const styles = {
+  divStyle: {
+    width: '100%',
+  },
+  labelStyle: {
+    display: 'block'
+  },
+  inputStyle: {
+    width: '100%',
+    border: '2px solid #222'
+  },
+  textareaStyle: {
+    width: '100%',
+    height: '132px',
+    border: '2px solid #222',
+    resize: 'none'
+  },
+  btnStyle: {
+    width: '80px',
+    height: '32px',
+    backgroundColor: '#89AD6D',
+    color: '#fff',
+    borderRadius: '4px'
+  }
+}
+
+// Export react component
+export default function Contact() {
+  return (
+    <form>
+      <div style={styles.divStyle}>
+        <label style={styles.labelStyle} for='name'>Name</label>
+        <input style={styles.inputStyle} id='name' name='name'></input>
+      </div>
+      <div style={styles.divStyle}>
+        <label style={styles.labelStyle} for='email'>Email Address</label>
+        <input style={styles.inputStyle} type='email' id='email' name='email'></input>
+      </div>
+      <div style={styles.divStyle}>
+        <label style={styles.labelStyle} for='message'>Message</label>
+        <textarea style={styles.textareaStyle} id='message' name='message'></textarea>
+      </div>
+      <div style={styles.divStyle}>
+        <button type="submit" style={styles.btnStyle}>Submit</button>
+      </div>
+    </form>
+  );
+}
