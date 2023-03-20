ecf3724f959a369c4c0153031a1a430aa77422d1
Joshua Taylor <toasterrage@gmail.com>
2023-03-19 18:46:34 -0600

    Moved some styling to component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,25 +6,47 @@ import emailIcon from '../assets/icons/email_icon.svg';
 import githubIcon from '../assets/icons/GitHub_icon.svg';
 import linkedinIcon from '../assets/icons/LinkedIn_icon.svg';
 
+// Component styling, pseudo-class and pseudo-element styling still in external file
+const styles = {
+  footerStyle: {
+    position: 'relative',
+    bottom: '10px'
+  },
+  ulStyle: {
+    marginTop: '8px',
+    height: '48px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    justifyItems: 'center',
+    alignItems: 'center'
+  },
+  imgStyle: {
+    height: '34px',
+    width: '34px',
+    objectFit: 'fill'
+  }
+}
+
 // Export react component
 export default function Footer() {
   return (
-    <footer id='contact-me'>
-      <ul>
+    <footer id='contact-me' style={styles.footerStyle}>
+      <ul style={styles.ulStyle}>
         <li id='phone' className='show-contact'>
-          <img src={phoneIcon} alt="phone icon"></img>
+          <img src={phoneIcon} alt="phone icon" style={styles.imgStyle}></img>
         </li>
         <li id='email' className='show-contact'>
-          <img src={emailIcon} alt="email icon"></img>
+          <img src={emailIcon} alt="email icon" style={styles.imgStyle}></img>
         </li>
         <li>
           <a href="https://github.com/josht-dev" target="_blank" rel="noreferrer">
-            <img src={githubIcon} alt="github icon"></img>
+            <img src={githubIcon} alt="github icon" style={styles.imgStyle}></img>
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/joshua-taylor-43ab14145" target="_blank" rel="noreferrer">
-            <img src={linkedinIcon} alt="linkedin icon"></img>
+            <img src={linkedinIcon} alt="linkedin icon" style={styles.imgStyle}></img>
           </a>
         </li>
       </ul>
